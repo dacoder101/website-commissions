b9efda356ae5f20265a71512ffbd9e4d5fd56192
dacoder <107454678+dacoder101@users.noreply.github.com>
2023-07-18 18:36:23 +0000

    Updated Backend

--- index.js
+++ index.js
@@ -8,6 +8,14 @@ IP = process.env.IP;
 app.use("/", express.static(path.join(__dirname, "views")));
 app.use("/public", express.static(path.join(__dirname, "public")));
 
+app.use("/pricing", (req, res) => {
+    res.sendFile(path.join(__dirname, "views", "pricing.html"));
+});
+
+app.use("/request", (req, res) => {
+    res.sendFile(path.join(__dirname, "views", "request.html"));
+});
+
 app.listen(PORT, IP, () => {
     console.log(`Listening on ${IP}:${PORT}`);
 });
